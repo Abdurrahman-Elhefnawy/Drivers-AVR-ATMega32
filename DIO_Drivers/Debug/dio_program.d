317c031274c0cef1729f4c145556fa4d54bc61a2
mahmoud481 <mah.mohamed48@gmail.com>
2021-12-18 18:30:32 +0200

    first commit

---
+++ DIO_Drivers/Debug/dio_program.d
@@ -0,0 +1,10 @@
+dio_program.o dio_program.o: ../dio_program.c ../dio_interface.h \
+  ../bit_math.h ../dio_registers.h ../std_types.h
+
+../dio_interface.h:
+
+../bit_math.h:
+
+../dio_registers.h:
+
+../std_types.h:
