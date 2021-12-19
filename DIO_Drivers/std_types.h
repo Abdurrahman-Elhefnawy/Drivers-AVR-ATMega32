736e801a31368b4395177a098dbc2b9dad90bea4
abdurrahman26 <abdurrahmanma01@gmail.com>
2021-12-19 13:34:07 +0200

    first commit

---
+++ DIO_Drivers/std_types.h
@@ -0,0 +1,20 @@
+/*
+ * std_types.h
+ *
+ *  Created on: Oct 2, 2021
+ *      Author: Abdurrahman Mohamed
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef char* pu8;
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short int u16;
+typedef signed short int s16;
+typedef unsigned int u32;
+typedef signed int s32;
+typedef unsigned long long u64;
+
+#endif /* STD_TYPES_H_ */
